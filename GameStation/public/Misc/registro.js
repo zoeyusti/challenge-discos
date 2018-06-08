699b19cab2699639bb275e6e6a940ea1b58ecce6
Zoe <zoeyusti@gmail.com>
2018-06-08 20:52:54 -0300

    navegacion entre paginas chequeo de contraseñas y llanto de zoe

--- GameStation/public/Misc/registro.js
+++ GameStation/public/Misc/registro.js
@@ -1,8 +1,14 @@
+var infoUsuarios = {};
+var Usuarios = [];
+var datos = localStorage.getItem('Usuarios');
+var passwordOK = false;
 
 
 
 document.getElementById('botonNoRegistrado').addEventListener("click", mostrarRegistrar);
 document.getElementById('botonRegistrado2').addEventListener("click", mostrarIngresar);
+document.getElementById('registrar').addEventListener("click", guardarInformacion);
+
 
 function mostrarRegistrar(){
 
@@ -16,3 +22,50 @@ function mostrarIngresar(){
 	document.getElementById('usuarioNoRegistrado').style.display="none";
 	
 }
+
+function chequearCont(){
+	var cont1 = document.getElementById('password1').value;
+	var cont2 = document.getElementById('password2').value;
+
+	if (cont1 != cont2) {
+		alert("Las contraseñas deben ser iguales!");
+	}
+	else{
+		passwordOK = true;
+	}
+
+}
+
+function guardarInformacion(){
+
+	chequearCont();
+
+	if (passwordOK == true) {
+
+		var emailUsuarios = document.getElementById('email').value;
+		var passwordUsuarios = document.getElementById('password').value;
+	    infoUsuarios = {user: email, pass: password}
+	    console.log(infoUsuarios);
+
+	    if(datos==null){
+	        Usuarios = [];
+	    }else{
+	        Usuarios = JSON.parse(datos).Usuarios; 
+	    }
+	    console.log(datos);
+	    Usuarios.push(infoUsuarios);
+	   
+
+	    let JASON = {'Usuarios':Usuarios,'total':Usuarios.length}
+	    console.log(JASON); //Jason cumple, Jason para presidente
+
+	    let informacion = JSON.stringify(JASON);
+	    
+	    localStorage.setItem('Usuarios', informacion);
+
+	    console.log(Usuarios);
+	}
+    
+
+}
+
