ba5924463df59adb620e57c396eea662c0d2451b
pinkywhale <erica_eli@hotmail.com.ar>
2018-06-08 12:47:54 -0300

    BDD en formato JSON

---
+++ GameStation/public/Misc/BDD.js
@@ -0,0 +1,12 @@
+//BDD data:
+var baseDD = localStorage.setItem(myJSON);
+
+var myGames = [
+    {"name":"Assassin Creed II", "genero":"", "cantidad": 2 },
+    {"name":"Batman", "genero":"", "cantidad": 5},
+    {"name":"Los Sims: Magia Potagia", "genero":"", "cantidad": 10},
+    {"name":"Prince of Persia: The Two Thrones", "genero":"", "cantidad": 7},
+    {"name":"Sonic", "genero":"", "cantidad": 2},
+    {"name":"The legend of Zelda: Twilight Princess", "genero":"", "cantidad": 6},
+]
+var myJSON = JSON.stringify(myGames);
