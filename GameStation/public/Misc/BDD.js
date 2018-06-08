46b975f5b2660f9236fc3d9fd4366025c8804992
pinkywhale <erica_eli@hotmail.com.ar>
2018-06-08 12:56:32 -0300

    JSON de base de datos

--- GameStation/public/Misc/BDD.js
+++ GameStation/public/Misc/BDD.js
@@ -2,11 +2,11 @@
 var baseDD = localStorage.setItem(myJSON);
 
 var myGames = [
-    {"name":"Assassin Creed II", "genero":"", "cantidad": 2 },
-    {"name":"Batman", "genero":"", "cantidad": 5},
-    {"name":"Los Sims: Magia Potagia", "genero":"", "cantidad": 10},
-    {"name":"Prince of Persia: The Two Thrones", "genero":"", "cantidad": 7},
-    {"name":"Sonic", "genero":"", "cantidad": 2},
-    {"name":"The legend of Zelda: Twilight Princess", "genero":"", "cantidad": 6},
+    {"name":"Assassin Creed II", "genero":"Aventura", "cantidad": 2 },
+    {"name":"Batman", "genero":"Aventura", "cantidad": 5},
+    {"name":"Los Sims: Magia Potagia", "genero":"Simulacion", "cantidad": 10},
+    {"name":"Prince of Persia: The Two Thrones", "genero":"Aventura", "cantidad": 7},
+    {"name":"Sonic", "genero":"Aventura", "cantidad": 2},
+    {"name":"The legend of Zelda: Twilight Princess", "genero":"Aventura", "cantidad": 6},
 ]
 var myJSON = JSON.stringify(myGames);
