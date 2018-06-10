db1a2007c22cb3a5938e3baa2e7b554689ad0198
Zoe <zoeyusti@gmail.com>
2018-06-10 16:43:28 -0300

    arreglos para el registro

--- GameStation/public/Misc/registro.js
+++ GameStation/public/Misc/registro.js
@@ -2,11 +2,12 @@ var infoUsuarios = {};
 var Usuarios = [];
 var datos = localStorage.getItem('Usuarios');
 var passwordOK = false;
+var emailOK = false;
 
 
 
 document.getElementById('botonNoRegistrado').addEventListener("click", mostrarRegistrar);
-document.getElementById('botonRegistrado2').addEventListener("click", mostrarIngresar);
+document.getElementById('botonRegistrado').addEventListener("click", mostrarIngresar);
 document.getElementById('registrar').addEventListener("click", guardarInformacion);
 
 
@@ -29,6 +30,8 @@ function chequearCont(){
 
 	if (cont1 != cont2) {
 		alert("Las contraseñas deben ser iguales!");
+	}else if (cont1 == "" || cont2 == "") {
+		alert("Debe tener una contraseña para poder registrarse!")
 	}
 	else{
 		passwordOK = true;
@@ -36,28 +39,46 @@ function chequearCont(){
 
 }
 
+function chequearEmail(){
+	var mail = document.getElementById('email').value;
+	var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+	if (emailRegex.test(mail)) {
+		//console.log("EMAIL "+ mail);
+		if (mail == "") {
+			alert("Debe tener un e-mail para registrarse!");
+		}else{
+			emailOK = true;
+		}
+	}else{
+		alert("Eso no es un mail!");
+	}
+
+}
+
 function guardarInformacion(){
 
+	chequearEmail();
 	chequearCont();
 
-	if (passwordOK == true) {
+
+	if (passwordOK == true && emailOK == true) {
 
 		var emailUsuarios = document.getElementById('email').value;
 		var passwordUsuarios = document.getElementById('password').value;
-	    infoUsuarios = {user: email, pass: password}
-	    console.log(infoUsuarios);
+	    infoUsuarios = {user: emailUsuarios, pass: passwordUsuarios}
+	    //console.log(infoUsuarios);
 
 	    if(datos==null){
 	        Usuarios = [];
 	    }else{
 	        Usuarios = JSON.parse(datos).Usuarios; 
 	    }
-	    console.log(datos);
 	    Usuarios.push(infoUsuarios);
 	   
 
 	    let JASON = {'Usuarios':Usuarios,'total':Usuarios.length}
-	    console.log(JASON); //Jason cumple, Jason para presidente
+	    //console.log(JASON);
 
 	    let informacion = JSON.stringify(JASON);
 	    
@@ -65,7 +86,7 @@ function guardarInformacion(){
 
 	    console.log(Usuarios);
 	}
-    
+//localStorage.clear();
 
 }
 
