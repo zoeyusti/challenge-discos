e4fc0c0559c2b449fa75f5921aabd8ba7572c13f
pinkywhale <erica_eli@hotmail.com.ar>
2018-06-12 12:01:58 -0300

    las loveo a todas Ya estamos en Express

--- GameStation/routes/index.js
+++ GameStation/routes/index.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bienvenidos' });
 });
 
 router.get('/conocenos', function(req, res, next) {
   res.render('conocenos', { title: 'Conocenos' });
+});
+
+router.get('/contacto', function(req, res, next) {
+  res.render('contacto', { title: 'Contacto' });
+});
+
+router.get('/registro', function(req, res, next) {
+  res.render('registro', { title: 'Registrarse' });
 });
 
 module.exports = router;
